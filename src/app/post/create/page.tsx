7a5c942463eefc2e18a606198a0794367caf515d
FYZ <63561158+Fyz-dev@users.noreply.github.com>
2023-11-23 16:15:03 +0200

    Finish customized MDXEditor

--- src/app/post/create/page.tsx
+++ src/app/post/create/page.tsx
@@ -9,6 +9,75 @@ import { Search } from 'src/assets/icons';
 import { MDXEditor } from 'src/components/MDXEditor';
 import { MDXRemote } from 'next-mdx-remote/rsc';
 
+const markdownContent: string = `
+# Welcome
+
+This is a **live demo** of MDXEditor with all default features on.
+
+> The overriding design goal for Markdown’s formatting syntax is to make it as readable as possible.
+> The idea is that a Markdown-formatted document should be publishable as-is, as plain text,
+> without looking like it’s been marked up with tags or formatting instructions.
+
+[— Daring Fireball](https://daringfireball.net/projects/markdown/).
+
+In here, you can find the following markdown elements:
+
+* Headings
+* Lists
+  * Unordered
+  * Ordered
+  * Check lists
+  * And nested ;)
+* Links
+* Bold/Italic/Underline formatting
+* Tables
+* Code block editors
+* And much more.
+
+The current editor content is styled using the \`@tailwindcss/typography\` [plugin](https://tailwindcss.com/docs/typography-plugin).
+
+## What can you do here?
+
+This is a great location for you to test how editing markdown feels. If you have an existing markdown source, you can switch to source mode using the toggle group in the top right, paste it in there, and go back to rich text mode.
+
+If you need a few ideas, here's what you can try:
+
+1. Add your own code sample
+2. Change the type of the headings
+3. Insert a table, add a few rows and columns
+4. Switch back to source markdown to see what you're going to get as an output
+5. Test the diff feature to see how the markdown has changed
+6. Add a frontmatter block through the toolbar button
+
+## A code sample
+
+MDXEditor embeds CodeMirror for code editing.
+
+\`\`\`tsx
+export default function App() {
+  return (<div>Hello world</div>)
+}
+\`\`\`
+
+## A live code example
+
+The block below is a live React component. You can configure multiple live code presets that specify the available npm packages and the default imports. You can also specify a default component that will be rendered in the live code block.
+
+\`\`\`jsx live
+export default function App() {
+  return (<div>
+  <p>This is a live React component, that's being previewed in codesandbox. </p>
+  <p>Editing it will update the fenced codeblock in the markdown.</p>
+  </div>)
+}
+\`\`\`
+
+## A table
+
+Play with the table below - add rows, columns, change column alignment. When editing,
+you can navigate the cells with \`enter\`, \`shift+enter\`, \`tab\` and \`shift+tab\`.
+`;
+
 const CreatePage: FC = () => {
   return (
     <div className="m-5 flex justify-center gap-5">
@@ -19,15 +88,13 @@ const CreatePage: FC = () => {
           </CardHeader>
           <CardBody className="flex gap-3">
             <Input variant="bordered" placeholder="Add a title..."></Input>
-            <MDXEditor markdown="Hello" placeholder="Add a desription..." />
-            <div className="prose dark:prose-invert">
-              <MDXRemote
-                source={`\`\`\`js
-export default function App() {
-  return (<div>Hello world</div>)
-}
-\`\`\``}
-              ></MDXRemote>
+            <MDXEditor
+              diffMarkdown={markdownContent}
+              markdown={markdownContent}
+              placeholder="Add a desription..."
+            />
+            <div className="prose max-w-none dark:prose-invert">
+              <MDXRemote source={markdownContent} />
             </div>
             <Checkbox name="isNSFW" color="danger">
               Is NSFW
--- src/components/MDXEditor/InitializedMDXEditor.tsx
+++ src/components/MDXEditor/InitializedMDXEditor.tsx
@@ -16,25 +16,36 @@ import {
   InsertCodeBlock,
   codeMirrorPlugin,
   CodeMirrorEditor,
+  InsertThematicBreak,
+  Separator,
+  linkPlugin,
+  linkDialogPlugin,
+  CreateLink,
+  ListsToggle,
 } from '@mdxeditor/editor';
 import { UndoRedo } from '@mdxeditor/editor/plugins/toolbar/components/UndoRedo';
 import { BoldItalicUnderlineToggles } from '@mdxeditor/editor/plugins/toolbar/components/BoldItalicUnderlineToggles';
 import '@mdxeditor/editor/style.css';
 import './MDXEditor.css';
 
-const InitializedMDXEditor = ({ ...props }: MDXEditorProps) => {
+const InitializedMDXEditor = ({
+  diffMarkdown,
+  ...props
+}: { diffMarkdown?: string } & MDXEditorProps) => {
   return (
     <div className="overflow-hidden rounded-medium border-2 border-default-200 transition-all focus-within:border-foreground">
       <MDXEditorOriginal
         autoFocus
         className="mdx-editor"
-        contentEditableClassName="!text-foreground !focus:border-default-500 prose dark:prose-invert"
+        contentEditableClassName="placeholder:text-foreground-500 !focus:border-default-500 prose max-w-none prose-a dark:prose-invert"
         plugins={[
           headingsPlugin({
             allowedHeadingLevels: [1, 2, 3, 4],
           }),
           listsPlugin(),
           quotePlugin(),
+          linkPlugin(),
+          linkDialogPlugin(),
           codeBlockPlugin({
             defaultCodeBlockLanguage: 'text',
             codeBlockEditorDescriptors: [
@@ -52,15 +63,21 @@ const InitializedMDXEditor = ({ ...props }: MDXEditorProps) => {
           }),
           thematicBreakPlugin(),
           markdownShortcutPlugin(),
-          diffSourcePlugin(),
+          diffSourcePlugin({ diffMarkdown: diffMarkdown }),
           toolbarPlugin({
             toolbarContents: () => (
               <>
                 <DiffSourceToggleWrapper>
                   <UndoRedo />
+                  <Separator />
                   <BoldItalicUnderlineToggles />
-                  <InsertCodeBlock />
+                  <ListsToggle />
+                  <Separator />
                   <BlockTypeSelect />
+                  <Separator />
+                  <CreateLink />
+                  <InsertCodeBlock />
+                  <InsertThematicBreak />
                 </DiffSourceToggleWrapper>
               </>
             ),
