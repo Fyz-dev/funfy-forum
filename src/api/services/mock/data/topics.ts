7eadfa1f38c9d4b366a61cc4db0ee2f0ea85defd
FYZ <63561158+Fyz-dev@users.noreply.github.com>
2023-11-24 23:17:13 +0200

    Fix: add photoURL for topic

--- src/api/services/mock/data/topics.ts
+++ src/api/services/mock/data/topics.ts
@@ -5,12 +5,16 @@ export const topics: ITopic[] = [
     id: 'topic1',
     userID: 'user1',
     title: 'Web Development Best Practices',
+    photoURL:
+      'https://b.thumbs.redditmedia.com/8RJ1zsSxLbTrSrRAhziwMynfkWVcuFNMXPsLqtGct1o.png',
     description:
       'Discuss and share the best practices for web development, including coding standards, design patterns, and optimization techniques.',
   },
   {
     id: 'topic2',
     userID: 'user9',
+    photoURL:
+      'https://builtin.com/cdn-cgi/image/f=auto,quality=80,width=752,height=435/https://builtin.com/sites/www.builtin.com/files/styles/byline_image/public/2021-12/machine-learning-examples-applications.png',
     title: 'Machine Learning Applications in Healthcare',
     description:
       'Explore the various applications of machine learning in healthcare, such as disease prediction, diagnostic tools, and personalized treatment plans.',
@@ -18,6 +22,8 @@ export const topics: ITopic[] = [
   {
     id: 'topic3',
     userID: 'user1',
+    photoURL:
+      'https://static-00.iconduck.com/assets.00/open-book-icon-2048x2048-wuklhx59.png',
     title: 'Book Recommendations for Science Enthusiasts',
     description:
       'Share and discover interesting books related to various scientific disciplines. Discuss your favorite science books and their impact on your understanding of the world.',
--- src/interface/ITopic.ts
+++ src/interface/ITopic.ts
@@ -1,6 +1,7 @@
 export interface ITopic {
   id: string;
   userID: string;
+  photoURL: string | null;
   title: string;
   description: string;
 }
