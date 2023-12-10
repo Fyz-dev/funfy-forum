4718e5d0f37dbc551f18fb22549fa7b0db9ef544
FYZ <63561158+Fyz-dev@users.noreply.github.com>
2023-12-11 00:46:29 +0200

    Refactoring functions

--- src/utils/getSortParam.ts
+++ src/utils/getSortParam.ts
@@ -17,21 +17,41 @@ const getSortParam = (searchParams: TSearchParams): string | undefined => {
   return Array.isArray(sortParamArray) ? sortParamArray[0] : sortParamArray;
 };
 
-export const getSortPostParam = (searchParams: TSearchParams): TSortPost => {
-  const searchParam = getSortParam(searchParams) as TSortPost;
-  return sortPost.includes(searchParam) ? searchParam : 'new';
+const getType = <T extends string>(
+  searchParam: string | undefined,
+  validSortValues: readonly T[],
+): T => {
+  return validSortValues.includes(searchParam as T)
+    ? (searchParam as T)
+    : validSortValues[0];
 };
+
+export const getSortPostParam = (searchParams: TSearchParams): TSortPost =>
+  getType(getSortParam(searchParams), sortPost);
 
 export const getSortCommentsUserParam = (
   searchParams: TSearchParams,
-): TSortCommentsUser => {
-  const searchParam = getSortParam(searchParams) as TSortCommentsUser;
-  return sortCommentsUser.includes(searchParam) ? searchParam : 'new';
-};
+): TSortCommentsUser => getType(getSortParam(searchParams), sortCommentsUser);
 
 export const getSortCommentsParam = (
   searchParams: TSearchParams,
-): TSortComments => {
-  const searchParam = getSortParam(searchParams) as TSortComments;
-  return sortComments.includes(searchParam) ? searchParam : 'new';
-};
+): TSortComments => getType(getSortParam(searchParams), sortComments);
+
+// export const getSortPostParam = (searchParams: TSearchParams): TSortPost => {
+//   const searchParam = getSortParam(searchParams) as TSortPost;
+//   return sortPost.includes(searchParam) ? searchParam : 'new';
+// };
+
+// export const getSortCommentsUserParam = (
+//   searchParams: TSearchParams,
+// ): TSortCommentsUser => {
+//   const searchParam = getSortParam(searchParams) as TSortCommentsUser;
+//   return sortCommentsUser.includes(searchParam) ? searchParam : 'new';
+// };
+
+// export const getSortCommentsParam = (
+//   searchParams: TSearchParams,
+// ): TSortComments => {
+//   const searchParam = getSortParam(searchParams) as TSortComments;
+//   return sortComments.includes(searchParam) ? searchParam : 'new';
+// };
