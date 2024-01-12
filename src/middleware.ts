c7fccf92f1c89974f8085c57e39fc7b9e539e3bc
FYZ <63561158+Fyz-dev@users.noreply.github.com>
2024-01-12 16:19:39 +0200

    Migration from supabase/ssr to supabase/auth-helpers-nextjs

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,62 +1,13 @@
-import { createServerClient, type CookieOptions } from '@supabase/ssr';
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function middleware(request: NextRequest) {
-  let response = NextResponse.next({
-    request: {
-      headers: request.headers,
-    },
-  });
+  let response = NextResponse.next();
 
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        get(name: string) {
-          return request.cookies.get(name)?.value;
-        },
-        set(name: string, value: string, options: CookieOptions) {
-          // If the cookie is updated, update the cookies for the request and response
-          request.cookies.set({
-            name,
-            value,
-            ...options,
-          });
-          response = NextResponse.next({
-            request: {
-              headers: request.headers,
-            },
-          });
-          response.cookies.set({
-            name,
-            value,
-            ...options,
-          });
-        },
-        remove(name: string, options: CookieOptions) {
-          // If the cookie is removed, update the cookies for the request and response
-          request.cookies.set({
-            name,
-            value: '',
-            ...options,
-          });
-          response = NextResponse.next({
-            request: {
-              headers: request.headers,
-            },
-          });
-          response.cookies.set({
-            name,
-            value: '',
-            ...options,
-          });
-        },
-      },
-    },
-  );
-
-  const { data } = await supabase.auth.getSession();
+  const { data } = await createMiddlewareClient({
+    res: response,
+    req: request,
+  }).auth.getSession();
 
   if (!data.session?.user) {
     return Response.json(
--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,9 +1,6 @@
-import { createBrowserClient as createBrowserClientBase } from '@supabase/ssr';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 import { Database } from 'src/types';
 
 // For client
 export const createBrowserClient = () =>
-  createBrowserClientBase<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  );
+  createClientComponentClient<Database>();
