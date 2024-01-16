9511fb064bb102e54f7272df39dd64d1af723007
FYZ <63561158+Fyz-dev@users.noreply.github.com>
2024-01-17 00:29:39 +0200

    Refactoring

---
+++ src/app/post/[id]/(components)/CommentCreateForm.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { FC } from 'react';
+import Link from 'next/link';
+import { toUser } from 'src/utils/paths';
+import CreateComment from './CreateComment';
+import { useAuth } from 'src/context/Auth';
+import { IPost } from 'src/interface';
+
+const CommentCreateForm: FC<{ post: IPost }> = ({ post }) => {
+  const { user } = useAuth();
+
+  return (
+    <div className="mb-4 flex w-full flex-col gap-1 overflow-hidden">
+      {user ? (
+        <>
+          <span>
+            Comment as{' '}
+            <Link className="text-primary" href={toUser(user.uid)}>
+              {user.name}
+            </Link>
+          </span>
+          <CreateComment post={post} />
+        </>
+      ) : (
+        <CreateComment post={post} />
+      )}
+    </div>
+  );
+};
+
+export default CommentCreateForm;
--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -3,9 +3,15 @@ import { notFound } from 'next/navigation';
 import { TSearchParams } from 'src/types';
 import { getSortCommentsParam } from 'src/utils';
 import { withTieToTop } from 'src/hoc';
-import CommentSection from './(components)/CommentSection';
 import { getCommentsByPost, getPostById } from 'src/api/supabase';
 import { CommentsTreeContextProvider } from 'src/context/CommentsTreeContext';
+import DropDownSort, {
+  CommentsSortConfig,
+} from 'src/components/ui/DropDownSort';
+import { Divider } from '@nextui-org/react';
+import { InfiniteCommentTree } from 'src/components/InfiniteScroll';
+import { Comments } from 'src/assets/icons';
+import CommentCreateForm from './(components)/CommentCreateForm';
 
 const getPost = async (id: string) => {
   try {
@@ -20,6 +26,12 @@ const PostPage: FC<{
   searchParams: TSearchParams;
 }> = async ({ params: { id }, searchParams }) => {
   const post = await getPost(id);
+  const comments = await getCommentsByPost(
+    post.id,
+    getSortCommentsParam(searchParams),
+    1,
+    5,
+  );
 
   return (
     <CommentsTreeContextProvider
@@ -31,7 +43,41 @@ const PostPage: FC<{
         return getCommentsByPost(post.id, sort, page, sizePage);
       }}
     >
-      <CommentSection post={post} />
+      <CommentCreateForm post={post} />
+      {comments.length !== 0 && (
+        <>
+          <div className="mr-auto flex items-center gap-1">
+            <span className="text-small text-default-500">Sort by: </span>
+            <DropDownSort
+              {...CommentsSortConfig}
+              classNames={{
+                trigger:
+                  'shadow-none transition-all w-[10rem] py-0 min-h-8 h-unit-8 rounded-full',
+              }}
+            />
+          </div>
+          <Divider />
+        </>
+      )}
+
+      {/* Comments */}
+      <div className="h-full w-full">
+        {comments.length !== 0 ? (
+          <>
+            <InfiniteCommentTree />
+          </>
+        ) : (
+          <div className="m-10 flex h-full flex-col items-center justify-center gap-3 text-default-500">
+            <Comments className="h-16 w-16" />
+            <div className="flex flex-col items-center gap-1">
+              <span className="text-medium">Nothing Here</span>
+              <span className="text-center text-medium">
+                You can be the first to tell your thoughts!
+              </span>
+            </div>
+          </div>
+        )}
+      </div>
     </CommentsTreeContextProvider>
   );
 };
